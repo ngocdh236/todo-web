35f7245c1d86884d31ad597e8557cb77e57ba8af
Doan Hong Ngoc <ngocdh236@gmail.com>
2019-08-02 01:57:56 +0300

    Moved setAuthToken into customAxios file

--- src/services/customAxios.js
+++ src/services/customAxios.js
@@ -3,3 +3,12 @@ import axios from 'axios'
 export const customAxios = axios.create({
   baseURL: 'http://muzify.eu/api'
 })
+
+export const setAuthToken = token => {
+  if (token) {
+    customAxios.defaults.headers.common['Authorization'] = `Bearer ${token}`
+    customAxios.defaults.headers.common['Content-Type'] = 'application/json'
+  } else {
+    delete customAxios.defaults.headers.common['Authorization']
+  }
+}
