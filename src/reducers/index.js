ab94f2df5344aa539179f60c9159104f45d10027
Doan Hong Ngoc <ngocdh236@gmail.com>
2019-08-02 01:57:09 +0300

    Completed removing redux

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import error from './errorReducer'
-import notification from './notificationReducer'
-
-export default combineReducers({
-  error,
-  notification
-})
--- src/services/notificationActions.js
+++
@@ -1,5 +0,0 @@
-import { Types } from '.'
-
-export const removeNotification = () => {
-  return { type: Types.REMOVE_NOTIFICATION }
-}
