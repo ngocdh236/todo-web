46f6826422ef996cb2df0976a12a493543f2fc51
Doan Hong Ngoc <ngocdh236@gmail.com>
2019-08-02 01:57:34 +0300

    Fixed isEmpty util for array

--- src/utils/isEmpty.js
+++ src/utils/isEmpty.js
@@ -1,5 +1,6 @@
 export const isEmpty = value =>
   value === undefined ||
   value === null ||
+  value.length === 0 ||
   (typeof value === 'object' && Object.keys(value).length === 0) ||
   (typeof value === 'string' && value.trim().length === 0)
