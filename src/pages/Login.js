a111ac4c35fd6ecb0514dbf42085a0783a77f1e0
Doan Hong Ngoc <ngocdh236@gmail.com>
2019-10-13 15:49:00 +0300

    Added credentials for demo

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,52 +1,52 @@
-import React, { useContext, useState } from 'react'
-import { Link, Redirect } from 'react-router-dom'
+import React, { useContext, useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
 
-import { AuthContext } from '../contexts/AuthContext'
+import { AuthContext } from '../contexts/AuthContext';
 
-import InputField from '../components/InputField'
+import InputField from '../components/InputField';
 
 export default function Login(props) {
-  const { auth, authService } = useContext(AuthContext)
+  const { auth, authService } = useContext(AuthContext);
 
   const { from } = props.location.state || {
     from: { pathname: '/' }
-  }
+  };
 
   if (auth.isAuthenticated) {
-    return <Redirect to={from} />
+    return <Redirect to={from} />;
   }
 
   const [inputValues, setInputValues] = useState({
-    usernameOrEmail: '',
-    password: ''
-  })
+    usernameOrEmail: 'demo',
+    password: 'demo12'
+  });
 
   function onChange(e) {
-    setInputValues({ ...inputValues, [e.target.name]: e.target.value })
+    setInputValues({ ...inputValues, [e.target.name]: e.target.value });
   }
 
   function onSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
 
     const userData = {
       usernameOrEmail: inputValues.usernameOrEmail,
       password: inputValues.password
-    }
+    };
 
-    authService.login(userData)
+    authService.login(userData);
   }
 
   return (
-    <div className='Login mt-5 text-center'>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-md-8 m-auto'>
-            <h1 className='display-4'>Log In</h1>
-            <form className='mt-5' noValidate onSubmit={onSubmit}>
+    <div className="Login mt-5 text-center">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 m-auto">
+            <h1 className="display-4">Log In</h1>
+            <form className="mt-5" noValidate onSubmit={onSubmit}>
               <InputField
-                placeholder='Username or Email Address'
-                name='usernameOrEmail'
-                type='text'
+                placeholder="Username or Email Address"
+                name="usernameOrEmail"
+                type="text"
                 value={inputValues.usernameOrEmail}
                 onChange={onChange}
                 error={
@@ -58,9 +58,9 @@ export default function Login(props) {
               />
 
               <InputField
-                placeholder='Password'
-                name='password'
-                type='password'
+                placeholder="Password"
+                name="password"
+                type="password"
                 value={inputValues.password}
                 onChange={onChange}
                 error={
@@ -69,12 +69,12 @@ export default function Login(props) {
                 }
               />
 
-              <input type='submit' className='btn btn-lg btn-light mt-5' />
+              <input type="submit" className="btn btn-lg btn-light mt-5" />
             </form>
             <br />
             <span onClick={() => authService.removeErrors()}>
-              <Link className='nav-link' to='/register'>
-                Already have an account? Click here
+              <Link className="nav-link" to="/register">
+                Don't have an account yet? Click here
               </Link>
             </span>
           </div>
@@ -82,9 +82,9 @@ export default function Login(props) {
         <br />
 
         {auth.error && (
-          <p className='lead text-danger text-center'>{auth.error}</p>
+          <p className="lead text-danger text-center">{auth.error}</p>
         )}
       </div>
     </div>
-  )
+  );
 }
